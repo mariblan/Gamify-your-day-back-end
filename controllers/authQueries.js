1c9d4a108170595c0fa6c51a59b5b91283913d31
Lilian Forger <lilianforger@gmail.com>
2022-07-01 14:37:23 +0200

    Breaking session for heroku deploy

--- controllers/authQueries.js
+++ controllers/authQueries.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res, next) => {
   try {
-    console.log(req.body);
     const {
       body: { name, email, password },
     } = req;
